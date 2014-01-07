0772a423a10aaa52c21bca7c0ac80d1ed30c9830
sachin.webonise <sachin.wagh@weboniselab.com>
2014-01-07 20:32:25 +0530

    Done with CRUD operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 MyApp::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :users 
+  root 'welcome#index'
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  # root 'users#index'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
